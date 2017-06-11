941113ecc2ae6506d07bf14de80da0d3469b6302
sergi10 <sergi.droid@gmail.com>
2017-06-11 18:44:22 +0200

    First commit

---
+++ PhoneApp/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Threading.Tasks;
+
+namespace PhoneApp
+{
+    [Activity(Label = "Phone App", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        static readonly System.Collections.Generic.List<string> PhoneNumbers = new System.Collections.Generic.List<string>();
+
+        protected async override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.UserDataLayout);
+            var Intent = new Android.Content.Intent(this, typeof(ValidateActivity));
+            StartActivity(Intent);
+
+
+            //  ValidateActivityButton.Click += (object sender, System.EventArgs e) =>
+            //{
+            //  var Intent = new Android.Content.Intent(this, typeof(ValidateActivity));
+            //StartActivity(Intent);
+            //  };           
+        }
+    }
+}
---
+++ PhoneApp/ValidateActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PhoneApp
+{
+    [Activity(Label = "@string/ValidateLab")]
+    public class ValidateActivity : Activity
+    {
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.UserDataLayout);
+
+            //Lab06A
+            var MessageView = FindViewById<TextView>(Resource.Id.ResultValidationTextView);
+            var EmailEntry = FindViewById<TextView>(Resource.Id.emailEntry);
+            var PasswordEntry = FindViewById<TextView>(Resource.Id.passwordEntry);
+            var ValidateButton = FindViewById<Button>(Resource.Id.ValidateButton);
+
+            var ServiceClient = new SALLab06.ServiceClient();
+
+            ValidateButton.Click += async (object sender, System.EventArgs e) =>
+            {
+                string myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+                var Result = await ServiceClient.ValidateAsync(EmailEntry.Text, PasswordEntry.Text, myDevice);
+
+                MessageView.Text = $"{ Result.Status}\n{ Result.Fullname}\n{ Result.Token}";
+               // MessageView.Text = "asdfbib \napshdfaosdnfon\n apsodfna \t 123";
+            };
+     
+        }
+    }
+}
